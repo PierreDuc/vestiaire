0c93388ee8e45b923b2832fe3fd7b699e89bd839
Poul Kruijt <poulkruijt@gmail.com>
2024-12-22 18:32:11 +0100

    properly buy stuff

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const urlOfProduct = 'https://us.vestiairecollective.com/women-bags/handbags/dolce-gabbana/multicolour-leather-dolce-gabbana-handbag-49978905.shtml';
+export const urlOfProduct = 'https://us.vestiairecollective.com/women-clothing/swimwear/versace/other-polyester-versace-swimwear-50857271.shtml';
 export const localUser = 'Danjel';
 export const clickPayButton = false;
