f4e48c44b2f9f6e380fbf173ccd44b73f8a2f67e
Poul Kruijt <poulkruijt@gmail.com>
2024-12-22 18:27:29 +0100

    properly buy stuff

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const urlOfProduct = 'https://us.vestiairecollective.com/women-bags/handbags/dolce-gabbana/multicolour-leather-dolce-gabbana-handbag-49978905.shtml';
+export const localUser = 'Danjel';
+export const clickPayButton = false;
--- tests/buy-stuff.spec.ts
+++ tests/buy-stuff.spec.ts
@@ -1,7 +1,6 @@
 import {test, chromium} from '@playwright/test';
+import {clickPayButton, localUser, urlOfProduct} from "../config";
 
-const urlOfProduct = 'https://us.vestiairecollective.com/women-bags/handbags/dolce-gabbana/multicolour-leather-dolce-gabbana-handbag-49978905.shtml';
-const localUser = 'pk85'
 const localChromeDirectory = `C:/Users/${localUser}/AppData/Local/Google/Chrome/User Data`;
 
 test('buy stuff', async () => {
@@ -19,13 +18,18 @@ test('buy stuff', async () => {
   while (true) {
     try {
       await page.click('[data-vc-dd-action-name="pdp_buy_button"]:not(:disabled)', { timeout: 500 });
-      await page.click('[data-vc-dd-action-name="proceed_to_checkout_from_cart_modal"]:not(:disabled)', { timeout: 1000 });
       break;
     } catch {
       await page.reload();
     }
   }
-  await page.waitForURL(/shop\/checkout/, { timeout: 1000 });
-  // await page.click('.paymentButtonsWrapper .paymentButton:not(:disabled)');
-  await new Promise((resolve) => setTimeout(resolve, 1000000));
+
+  await page.click('[data-vc-dd-action-name="proceed_to_checkout_from_cart_modal"]:not(:disabled)', { timeout: 15000 });
+  await page.waitForURL(/shop\/checkout/, { timeout: 15000 });
+
+  if (clickPayButton) {
+    await page.click('.paymentButtonsWrapper .paymentButton:not(:disabled)');
+  }
+
+  await new Promise(() => {});
 });
